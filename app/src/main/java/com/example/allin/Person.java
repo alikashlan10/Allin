8e2d204f13ac6f527f9a171be2608bf9d5708b9a
zidan <aliKashlan636@gmail.com>
2023-12-08 23:12:12 +0200

    Implement Factory DB classes structures only
    -- added three new classes --
    Person : abstract
    Admin : extends Person
    PersonFactory : creates Person
    
    -- edited user class --
    included all user info
    
    -- add UserAddress class --
    to hold address info

---
+++ app/src/main/java/com/example/allin/Person.java
@@ -0,0 +1,41 @@
+package com.example.allin;
+
+public abstract class Person {
+
+    String UserName;
+    String password;
+    int PersonID;
+
+    public abstract void login();
+
+
+    ////////////////////////// Setters and Getters //////////////////////////
+    //----------------------------------------------------------------------
+
+    public String getUserName() {
+        return UserName;
+    }
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public int getPersonID() {
+        return PersonID;
+    }
+    public void setPersonID(int personID) {
+        PersonID = personID;
+    }
+
+    /////////////////////////////////////////////////////////////////////////
+    //----------------------------------------------------------------------
+
+}
