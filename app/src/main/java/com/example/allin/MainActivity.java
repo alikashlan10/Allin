6828aae9ddaeb62db95cd7893148b55a3df3e26a
alikashlan1 <aliKashlan636@gmail.com>
2023-12-12 14:38:23 +0200

    adding item functionality (function in admin class) associated database insertion function

--- app/src/main/java/com/example/allin/MainActivity.java
+++ app/src/main/java/com/example/allin/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.allin;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
         DbHelper dbHelper=new DbHelper(this);
         Person u = PersonFactory.GetPerson("user");
         u.login("a","b");
+        Person k=new User();
+
+
 
 
     }
