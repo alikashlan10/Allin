20f2bffcf1be1fb2816e8082b4e7fe2ecae0f087
SamehKhaledMoh <sameh01125879@gmail.com>
2023-12-15 17:59:58 +0200

    khod ya kandeelooo

--- app/src/main/java/com/example/allin/MainActivity.java
+++ app/src/main/java/com/example/allin/MainActivity.java
@@ -3,8 +3,19 @@ package com.example.allin;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +26,20 @@ public class MainActivity extends AppCompatActivity {
 
         DbHelper dbHelper = new DbHelper(this);
         dbHelper.insertDummyUserData();
+        dbHelper.insertDummyItem();
         OnlineShoppingSystem system = OnlineShoppingSystem.getInstance();
         system.loadUsersFromDatabase(dbHelper);
+        system.InitializeAppData(dbHelper);
+        Item i = new Item();
+        ArrayList<String> kandeel = new ArrayList<>();
+        kandeel.add("Ahmed");
+        kandeel.add("Kandeelo");
+        i.setItemName("Iphone 15 pro ekhs");
+        system.getItemsList().add(i);
+        ListView lv = findViewById(R.id.testinglist);
+        //ItemAdapter ouradapter = new ItemAdapter(kandeel);
+        ArrayAdapter<String> testingadapter =new ArrayAdapter<String>(this,R.layout.activity_main,kandeel);
+        lv.setAdapter(testingadapter);
 
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
     }
 }
