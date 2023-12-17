49c9aa72567b3e31104b8357ba0797325e65c5fb
alikashlan1 <76254631+modric10260@users.noreply.github.com>
2023-12-18 00:30:39 +0200

    admin to add items

---
+++ app/src/main/java/com/example/allin/AdminToAddItems.java
@@ -0,0 +1,20 @@
+package com.example.allin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class AdminToAddItems extends AppCompatActivity {
+
+
+
+
+}
