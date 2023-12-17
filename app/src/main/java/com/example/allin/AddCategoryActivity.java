50756314b26f13e06d7aea98be94e9867406dd5b
Ahmed Kandeel <1kandeel1@gmail.com>
2023-12-17 22:14:52 +0200

    add & edit categories

--- app/src/main/java/com/example/allin/AddCategoryActivity.java
+++ app/src/main/java/com/example/allin/AddCategoryActivity.java
@@ -2,7 +2,12 @@ package com.example.allin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AddCategoryActivity extends AppCompatActivity {
 
@@ -10,5 +15,40 @@ public class AddCategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_category);
+        //get "categoryName" from "CategoryAdapter" in EDIT case
+        Intent intent = getIntent();
+        String categoryName = intent.getStringExtra("categoryName");
+
+        EditText addCategoryEditText = findViewById(R.id.addCategoryEditText);
+        //EDIT case
+        if(categoryName != null)
+            addCategoryEditText.setHint(categoryName);
+        Button addCategorySubmitButton = findViewById(R.id.addCategorySubmitButton);
+
+        OnlineShoppingSystem system = OnlineShoppingSystem.getInstance();
+        DbHelper dbHelper = new DbHelper(this);
+        addCategorySubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(categoryName == null){ //ADD case
+                    Category newCategory = new Category(addCategoryEditText.getText().toString());
+                    int categoryID = (int)dbHelper.insertNewCategory(newCategory);
+                    newCategory.setCategoryId(categoryID);
+                    system.getCategories().add(newCategory);
+                    Intent intent = new Intent(AddCategoryActivity.this, CategoryActivity.class);
+                    startActivity(intent);
+                }
+                else{ //EDIT case
+                    Category toBeEditCategory = system.getCategoryIdByName(categoryName);
+                    toBeEditCategory.setCategoryName(addCategoryEditText.getText().toString());
+                    //dbHelper.updateCategory();
+                }
+                //go back to "CategoryActivity"
+                Intent intent = new Intent(AddCategoryActivity.this, CategoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
